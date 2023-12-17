f11899d0c4e3f59a738897e580254be0e3546700
EricRequena <eric.requena.7e7@itb.cat>
2023-12-17 17:59:08 +0100

    Funcion Cuadrado closes #1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ namespace Exercici4
     {
         public static void Main()
         {
+            public static int Cuadrat(int OneVal)
+            {
+                int resultat;
 
+                resultat = OneVal * OneVal;
+                return resultat;
+            }
         }
     }
 }
